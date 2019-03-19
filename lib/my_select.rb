2124afd83978e4f1c8f6bbb685a1b339400cf6b4
Marcus Mellor <marcus@mellor.com>
2019-03-19 16:10:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,14 @@
 def my_select(collection)
  # your code here!
- 
+  new_array=[]
+  if block_given?
+    counter=0;
+    while counter<array.length
+      if yield(array[counter])
+        new`_array << array[counter]
+      end
+      counter+=1
+    end
+  end
+  new_array
 end
