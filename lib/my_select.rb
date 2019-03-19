c97b4f2547000101d4d2ae4330abf43afa35f408
Marcus Mellor <marcus@mellor.com>
2019-03-19 16:14:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
   new_array=[]
   if block_given?
     counter=0;
     while counter<array.length
       if yield(array[counter])
-        new`_array << array[counter]
+        new_array << array[counter]
       end
       counter+=1
     end
